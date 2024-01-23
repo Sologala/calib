f532c16155b04324b7f4dbba6223f47788c17df6
WenDawson <dawsonwen123@gmail.com>
2024-01-22 18:58:46 -0800

    reconstruct utils foldero

---
+++ src/utils/include/calib/utils/filesystem.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <fstream>
+#include <string>
+namespace calib
+{
+namespace filesys
+{
+
+inline bool file_exist(std::string &file_path)
+{
+    std::ifstream in(file_path);
+    if (in.is_open())
+    {
+        return true;
+    }
+    return false;
+}
+
+#if __WIN32__
+#include <direct.h>
+inline bool makedir(const std::string &file_path)
+{
+    return _mkdir(file_path.data());
+}
+
+#elif __LINUX__
+#include <sys/stat.h>
+inline bool makedir(const std::string &file_path)
+{
+    return mkdir(file_path.c_str(), 0777);
+}
+#endif
+
+#if __LINUX__
+// #define USE_BOOST_FILESYSTEM 1
+// #ifdef USE_BOOST_FILESYSTEM
+#include <boost/filesystem.hpp>
+inline bool makedir(const std::string &file_path)
+{
+    boost::filesystem::path dirPath(file_path);
+    return boost::filesystem::create_directory(dirPath);
+}
+#endif //
+} // namespace filesys
+
+} // namespace calib
---
+++ src/utils/include/calib/utils/logger/logger.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+#define LOGGER_SPDLOG
+#ifdef LOGGER_SPDLOG
+#include <spdlog/spdlog.h>
+
+#define LOGI(...)  SPDLOG_INFO(__VA_ARGS__);
+#define LOGW(...)  SPDLOG_WARN(__VA_ARGS__);
+#define LOGE(...)  SPDLOG_ERROR(__VA_ARGS__);
+
+#else
+#define LOGI(...) void(0);
+#define LOGW(...) void(0);
+#define LOGE(...) void(0);
+#endif
